ac42d4f483a78fdb652e0c063e2a708efcbc5532
Reed Chorn <reedchorn@Reeds-MacBook-Pro.local>
2018-06-05 17:12:37 -0700

    transitional issues

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import './App.css';
+import characters from './characters.json'
+import Wrapper from './components/Wrapper'
+import Navbar from './components/Navbar'
+import Title from './components/Title'
+import CharacterCard from './components/CharacterCard'
+
+class App extends Component {
+    state = {
+        message: "Click an image to begin!",
+        topScore: 0,
+        curScore: 0,
+        characters: characters,
+        unselectedCharacters : characters
+    }
+
+    componentDidMount() {
+    }
+
+    // shuffles after click
+    shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    // looks through list of characters to see which haven't been selected
+    selectCharacter = name => {
+        const findCharacter = this.state.unselectedCharacters
+        .find(item => item.name === name);
+
+        if(findCharacter === undefined) {
+            // failure to select a new character
+            this.setState({ 
+                message: "You guessed incorrectly!",
+                topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+                curScore: 0,
+                characters: characters,
+                unselectedCharacters
+                : characters
+            });
+        }
+        else {
+            // success to select a new character
+            const newCharacters = this.state.unselectedCharacters
+            .filter(item => item.name !== name);
+            
+            this.setState({ 
+                message: "You guessed correctly!",
+                curScore: this.state.curScore + 1,
+                characters: characters,
+                unselectedCharacters
+                : newCharacters
+            });
+        }
+
+        this.shuffleArray(characters);
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Navbar
+                    message={this.state.message}
+                    curScore={this.state.curScore}
+                    topScore={this.state.topScore}
+                />
+                <Title />
+                {
+                    this.state.characters.map(character => (
+                        <CharacterCard
+                            name={character.name}
+                            image={character.image}
+                            selectCharacter={this.selectCharacter} 
+                            curScore={this.state.curScore}
+                        />
+                    ))
+                }
+            </Wrapper>
+        );
+    }
+}
+
+export default App;
+
---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Title.css";
+
+const Title = props => (
+    <div className="titlebox">
+        <h1>Bob's Burgers Clicker Game!</h1>
+        <p>Click on a character to advance, but don't click the same character twice!</p>
+    </div>
+);
+
+export default Title;
+
